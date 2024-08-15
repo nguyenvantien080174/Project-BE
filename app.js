72ea7f1738e4c9c355e7e348d3b1a9fb252153d9
Nguyen Van Tien <nguyenvantien080174@gmail.com>
2024-08-15 19:54:23 +0700

    update api admin and requesteditor

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
-
 const app = express();
 const PORT = 3000;
+
+const admin = require ('./src/routes/admin');
+const RequestEditor = require('./src/routes/request-editor.router')
 
 app.get('/', (req, res) => {
     res.send('Hello world!!!!');
 })
+app.use('/api', admin);
+app.use('/api', RequestEditor);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
